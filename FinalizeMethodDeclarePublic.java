287cb06628647d9f91045fa6dda723c3864ff52b
komalThombare1 <100417869+komalThombare1@users.noreply.github.com>
2022-04-06 18:30:13 +0530

    Update FinalizeMethodDeclarePublic.java

--- FinalizeMethodDeclarePublic.java
+++ FinalizeMethodDeclarePublic.java
@@ -1,7 +1,7 @@
 package javacodechecker;
 import java.applet.Applet; 
 
-public final class FinalizeMethodDeclarePublic extends Applet {
+public class FinalizeMethodDeclarePublic extends Applet {
 	// EMB-ISSUE: CodeIssueNames.FINALIZE_METHOD_DECLARE_PUBLIC
 	public void finalize() {
 		} 
