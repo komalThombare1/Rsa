42c0843f09db581841031304d885a9cadd45e884
komalThombare1 <100417869+komalThombare1@users.noreply.github.com>
2022-04-06 15:13:29 +0530

    Update PublicMethodReturnPrivateDataStructure.java

--- PublicMethodReturnPrivateDataStructure.java
+++ PublicMethodReturnPrivateDataStructure.java
@@ -3,14 +3,14 @@ package javacodechecker;
 public class PublicMethodReturnPrivateDataStructure {
 	private static final String[] flower = {"rose,lotus"};
 	private String[] color = {"black","green"};
-	private int[] arr= {1,2,3};
+	public int[] arr= {1,2,3};
 	//EMB-ISSUE: CodeIssueNames.PUBLIC_METHOD_RETURN_PRIVATE_DATA_STRUCTURE
 	public String[] getColor() {
 	return color;
 	}
 	//EMB-ISSUE: CodeIssueNames.PUBLIC_METHOD_RETURN_PRIVATE_DATA_STRUCTURE
 		public String[] getColorss() {	
-		return this.color;
+		return Arrays.copyOf(color);
 		}
 	//EMB-ISSUE: CodeIssueNames.PUBLIC_METHOD_RETURN_PRIVATE_DATA_STRUCTURE/no-detect
 	public String[] getColors() {
