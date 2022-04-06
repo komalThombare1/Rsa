c4a6b7ab3ae262d12c7340529bf6deeffd178d6e
komalThombare1 <100417869+komalThombare1@users.noreply.github.com>
2022-04-06 18:23:28 +0530

    Create FinalizeMethodDeclarePublic.java

---
+++ FinalizeMethodDeclarePublic.java
@@ -0,0 +1,9 @@
+package javacodechecker;
+import java.applet.Applet; 
+
+public final class FinalizeMethodDeclarePublic extends Applet {
+	// EMB-ISSUE: CodeIssueNames.FINALIZE_METHOD_DECLARE_PUBLIC
+	public void finalize() {
+		} 
+}
+
