98eb34c352dd9c4d3db0cb72958519b9850c1eee
komalThombare1 <100417869+komalThombare1@users.noreply.github.com>
2022-04-06 14:51:22 +0530

    Update AvoidUsingRSAAlgorithmWithoutOAEP.java

--- AvoidUsingRSAAlgorithmWithoutOAEP.java
+++ AvoidUsingRSAAlgorithmWithoutOAEP.java
@@ -8,7 +8,7 @@ public class AvoidUsingRSAAlgorithmWithoutOAEP {
         Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithMD5AndMGF1Padding");
 
         // EMB-ISSUE: CodeIssueNames.AVOID_USING_RSA_ALGORITHM_WITHOUT_OAEP
-        Cipher rsa = javax.crypto.Cipher.getInstance("RSA/NONE/NoPadding");
+        Cipher rsa = javax.crypto.Cipher.getInstance("RSA/ECB/OAEPWithMD5AndMGF1Padding");
 
         // EMB-ISSUE: CodeIssueNames.AVOID_USING_RSA_ALGORITHM_WITHOUT_OAEP/no-detect
         Cipher cip = Cipher.getInstance("RSA/ECB/OAEPWithMD5AndMGF1Padding");
